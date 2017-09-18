86dd062280af9a7138748d72c65d11f7465b4592
Johanna Staedel <bluecherryme@gmail.com>
2017-09-18 21:49:01 +0200

    show card component done

--- src/ShowCard.jsx
+++ src/ShowCard.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 
-const ShowCard = props =>(
-  
-    <div className="show-card">
-      <img
-        src='./img/1.jpg'
-        alt={`${props.show.title} Show Poster`}
-      />
-      <div>
-        <h3>{props.show.title}</h3>
-        <h4>({props.show.year})</h4>
-        <p>{props.show.description}</p>
-      </div>
+const ShowCard = props => (
+  <div className="show-card">
+    <img src={require(`./img/posters/${props.show.poster}`)} alt={`${props.show.title} Show Poster`} />
+    <div>
+      <h3>{props.show.title}</h3>
+      <h4>({props.show.year})</h4>
+      <p>{props.show.description}</p>
     </div>
-  );
+  </div>
+);
 
 export default ShowCard;
